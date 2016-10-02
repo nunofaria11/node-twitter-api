ceded5719c568daee0f41acb1bbbd9082d1ced93
Nuno Faria <nunofaria11@gmail.com>
2016-10-02 17:04:53 +0100

    twitter api favorites list

--- app.js
+++ app.js
@@ -7,21 +7,44 @@ require('dotenv').config({
     silent: true
 });
 
-// TODO: Load twitter keys from env
-var apiKey = process.env.TWITTER_API_KEY;
+
 var port = process.env.PORT || 3000;
 
+// Load twitter keys from env
 var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 app.get('/', function(req, res) {
-    res.send('Hello World! ' + apiKey);
+    res.send('Sup?');
+});
+
+app.get('/twitter/favorites/list', function(req, res) {
+	// Access twitter client
+    client.get('favorites/list', function(error, tweets, response) {
+    	// Send error if occurred
+        if (error) {
+            res.status(500).send({
+                error: error
+            });
+            return;
+        }
+
+        // Respond with data
+        if (response.statusCode === 200) {        	
+            res.json(tweets);
+        } else {
+            res.status(500).send({
+                error: "Invalid status " + response.statusCode
+            });
+        }
+
+    });
 });
 
 app.listen(port, function() {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port ' + port + '!');
 });
