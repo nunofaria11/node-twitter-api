211f7040c50bbd1d5881c5dd95de90d958a75991
Nuno Faria <nunofaria11@gmail.com>
2016-10-02 16:42:37 +0100

    changes port to heroku

--- app.js
+++ app.js
@@ -2,24 +2,26 @@ var express = require('express');
 var app = express();
 var Twitter = require('twitter');
 
-/*require('dotenv').config({
-    path: ".config_.env"
-});*/
+require('dotenv').config({
+    path: ".config.env",
+    silent: true
+});
 
 // TODO: Load twitter keys from env
 var apiKey = process.env.TWITTER_API_KEY;
+var port = process.env.PORT || 3000;
 
-/*var client = new Twitter({
+var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});*/
+});
 
 app.get('/', function(req, res) {
     res.send('Hello World! ' + apiKey);
 });
 
-app.listen(3000, function() {
+app.listen(port, function() {
     console.log('Example app listening on port 3000!');
 });
