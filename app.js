e7a10dcb7080aa53d723f17b7f0f5e86f14c3092
Nuno Faria <nunofaria11@gmail.com>
2016-10-02 16:20:38 +0100

    adds twitter client

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var app = express();
+var Twitter = require('twitter');
 
 require('dotenv').config({
-	path: ".config.env"
+    path: ".config.env"
 });
 
 // TODO: Load twitter keys from env
 var apiKey = process.env.TWITTER_API_KEY;
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 app.get('/', function(req, res) {
     res.send('Hello World! ' + apiKey);
